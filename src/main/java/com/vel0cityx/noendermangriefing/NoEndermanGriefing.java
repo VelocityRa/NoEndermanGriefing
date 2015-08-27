1f84daac3950be3e8d4135e6574ffc4eb38328f9
Nick Renieris <makren67@gmail.com>
2015-08-27 15:28:46 +0300

    Update NoEndermanGriefing.java

--- src/main/java/com/vel0cityx/noendermangriefing/NoEndermanGriefing.java
+++ src/main/java/com/vel0cityx/noendermangriefing/NoEndermanGriefing.java
@@ -1,9 +1,8 @@
 package com.vel0cityx.noendermangriefing;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = NoEndermanGriefing.MODID, name = NoEndermanGriefing.MODNAME,version = NoEndermanGriefing.VERSION)
 public class NoEndermanGriefing
@@ -12,7 +11,7 @@ public class NoEndermanGriefing
     public static final String MODNAME = "No Enderman Griefing";
     public static final String VERSION = "1.0";
     
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(new EventHookContainerClass());
