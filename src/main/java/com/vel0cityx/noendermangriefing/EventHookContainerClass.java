28739b7b9767d2345212c10aac6e760884fa49e8
Nick Renieris <makren67@gmail.com>
2015-08-27 15:28:04 +0300

    Update EventHookContainerClass.java

--- src/main/java/com/vel0cityx/noendermangriefing/EventHookContainerClass.java
+++ src/main/java/com/vel0cityx/noendermangriefing/EventHookContainerClass.java
@@ -1,10 +1,9 @@
 package com.vel0cityx.noendermangriefing;
 
-import net.minecraft.entity.ai.*;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.monster.EntityEnderman;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventHookContainerClass
 {
@@ -16,16 +15,7 @@ public class EventHookContainerClass
     public void onEntityJoinWorld(EntityJoinWorldEvent event) {
         if (event.entity instanceof EntityEnderman) {
             EntityEnderman enderman = (EntityEnderman) event.entity;
-
-            //Remove all tasks (since we don't have access to the ones that we want)
-            enderman.tasks.taskEntries.clear();
-
-            //Re-add the ones that we want
-            enderman.tasks.addTask(0, new EntityAISwimming(enderman));
-            enderman.tasks.addTask(2, new EntityAIAttackOnCollide(enderman, 1.0D, false));
-            enderman.tasks.addTask(7, new EntityAIWander(enderman, 1.0D));
-            enderman.tasks.addTask(8, new EntityAIWatchClosest(enderman, EntityPlayer.class, 8.0F));
-            enderman.tasks.addTask(8, new EntityAILookIdle(enderman));
+            enderman.func_146081_a(Blocks.end_portal);
         }
     }
 }
